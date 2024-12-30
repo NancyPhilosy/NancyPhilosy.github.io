184ccb1412d4c53371958356c6fbe70cd1441094
NancyPhilosy <nancytmg17@gmail.com>
2024-12-30 14:27:17 +0545

    jjj

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// let temperature = prompt("Enter Your Temperature");
+
+// if (temperature < 20) {
+//     console.log('Very hot')
+// } else if(temperature >= 20 && temperature < 40) {
+//     console.log('hot')
+// } else if(temperature >= 40 && temperature < 60) {
+//     console.log('normal')
+// } else if(temperature >= 60 && temperature < 80) {
+//     console.log('cold')
+// } else if(temperature >= 80 && temperature < 100) {
+//     console.log('hot')
+// } else {
+//     console.log('invalid temperature')
+// }
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let person = {
+//     name: 'saidikshya tamang',
+//     age: 20,
+//     address: 'Dharan',
+// }
+
+// let {name, age, address} = person;
+
+// console.log(name)
