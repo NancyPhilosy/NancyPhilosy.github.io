77a4e7ad63e06ceedf8bc60f09c7a8fa6cc40e69
NancyPhilosy <nancytmg17@gmail.com>
2024-12-31 13:54:40 +0545

    mm

--- js/index.js
+++ js/index.js
@@ -13,6 +13,19 @@
 // } else {
 //     console.log('invalid temperature')
 // }
+
+// let points = prompt("Your Points");
+
+// if (points < 20) {
+//     console.log('You are not qualified')
+// } else if (points >= 20 && points < 40) {
+//     console.log('You are selected')
+// } else if (points >= 40 && points <  50  ) {
+//     console.log('You are qualified')
+// } else {
+//     console.log('Invalid Points')
+// }
+
 // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // let person = {
@@ -24,3 +37,13 @@
 // let {name, age, address} = person;
 
 // console.log(name)
+// console.log(age)
+
+// 
+
+// let a = prompt("Multiplication");
+
+// for (i = 1; i <= 10; i++){
+//     console.log(a + "x" + i + "=" + a * i)
+//     console.log(`${a} x ${i} = ${a*i}`) (mostly use)
+// }
